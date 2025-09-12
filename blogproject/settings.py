90b68660e085801cad67d8ad833da93542e2b6fc
Raghu Ram <203454950+raghuram-007@users.noreply.github.com>
2025-09-12 11:32:36 +0530

    upgrade

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -125,11 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    BASE_DIR/'static',
-    BASE_DIR/'blog/static'
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # points to the folder we just created
+STATIC_ROOT = BASE_DIR / "staticfiles"    # for collectstatic when deploying
+
 
 
 # Default primary key field type
