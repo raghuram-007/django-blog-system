beda4ab9d1c5b87377946f6a83a6c0d321f2a4b0
Raghu Ram <203454950+raghuram-007@users.noreply.github.com>
2025-09-21 17:27:06 +0530

    Add professional README with badges, usage, and folder structure

--- blog/views.py
+++ blog/views.py
@@ -13,8 +13,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
 from django.contrib.sites.shortcuts import get_current_site
-from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
+from django.template.loader import render_to_string
+
 from django.core.mail import send_mail
 from django.conf import settings
 from django.db.models import Q
